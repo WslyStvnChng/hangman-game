231a8f3336c799f37af92d9bbedd4e5baca23ddd
Steven^C <mr.cheng.st@gmail.com^C>
2017-12-05 03:10:31 -0700

    html&css done

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+
+
+// <script type="text/javascript">
+ 
+ 
+  
+//   </script>
+
+
+ {/* Pseudocode */}
+  //Generate a word at random and store as a variable
+  //Display the length of the word to user
+  //Correct_guess is less than the length of word  
+  //Prompt user to guess a letter
+  //Player choose a letter
+    if guess.length == 1 (the character)
+      if correct add letter to hidden word
+      if false substract from count of 10
+      display error for repeated guesses or not applicable
+  //If guess is correct increase by 1 correct_guesses by 1 
+  //If incorrect_guesses is greater than 10, tell user they lost
+  //If correct_guesses is equal to the length of word; tell user they won 
+
