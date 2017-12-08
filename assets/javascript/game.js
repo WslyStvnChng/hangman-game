3391f5c2574aedb95f7edd54119cefe33b361823
Steven^C <mr.cheng.st@gmail.com^C>
2017-12-07 18:33:30 -0700

    headache starts now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,34 @@
-<script type="text/javascript">
+//Global Event Handler to load event of a Window
+window.onload = function() {
 
 // Global Variables
-
-  //Variables
-  var wordBank = ['trout' ,'salmon', 'walleye', 'saugeye', 'bluegill', 'bass', 'crappie', 'sunfish', 'catfish'];  
-  var wins = 0;
-  var loss = 0;
+  var wordBank = ["trout", 'salmon', 'walleye', 'saugeye', 'bluegill', 'bass', 'crappie', 'sunfish', 'catfish'];
+  var wins = 0; //Starts game with 0 points
+  var loss = 0; //Ends when user losses
   var wrongLetter = [];
-  var guessLeft = 9;
-  var userGuesses = [];
-  var underScores = [];
+  var guessLeft = 10;
+  var userGuesses = []; //User Guesses
+  var underScores = []; //Pushes the Underscores
   var randomWord;
-
-  // Generate random word
   
-  function startGame() {
-  var wordBank = wordBank[Math.floor(Math.random()*wordBank.length)];
-  console.log(wordBank.length);
 
-    for(var i = 0; i < wordBank.length; i++) {
-      console.log(wordBank[i]);
-      
-        underScores.push('_');
-    }
-    document.getElementById('missing-word').innerHTML = underScores;
+// Generate random word
+  var randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+  console.log(randomWord);  //Words are randomly created in console
+
+//Select word to a variable
+
+  for(var i = 0; i < wordBank.length; i++) {
+    console.log(wordBank[i]);
+    underScores.push('_');
   }
+
+//Grab randomWord to place onto page with ID
+  var randomWord = document.getElementById('missing-word').textContent = randomWord;
   
 
-
-
-
-  
-
-
-
-</script>
-</body>
-</html>
-
+// Captures keyboard input. Depending on the letter pressed it will "call" (execute) different functions.
+//   document.onkeyup = function () {
+//   userGuesses = event.key();
+// }
+}
