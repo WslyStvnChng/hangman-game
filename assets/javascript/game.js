ac9f62fb3321d081a86125680c8f160ed3e69cd3
Steven^C <mr.cheng.st@gmail.com^C>
2017-12-09 23:55:45 -0700

    Continued on for loop creating if the randomword[i[ is equal to user's input. From there we made the underscores equal to the user guess. They I used an else statement if the statement above isn wrong, it will decrease the lives the user has starting from 10

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,33 +27,73 @@ window.onload = function() {
   // Create new variable that takes the randomWord selected and breaks into a array
 
   randomWordSplit = Array.from(randomWord);
-  console.log(randomWordSplit);
+  // console.log(randomWordSplit);
 
   //Select word to a variable for blanks
   for (var i = 0; i < randomWord.length; i++) {
     // console.log(wordBank[i]);
     underScores.push("_");
-  }
+  } //end for loop
 
   //Print underScores to screen
-  document.getElementById("missing-word").textContent = underScores.join(" ");
+  document.getElementById("missing-word").innerHTML = underScores.join(" ");
 
   //Now we grab a function in the event of user's guess
-  document.onkeyup = function(event) {
-    var keyPressed = event.key;
+  document.onkeyup = function(e) 
+  {
+    var userGuesses = e.key;  //user Guesses on keyboard
+    // console.log(keyPressed);
 
-    for (var i = 0; i < randomWord.length; i++) {
-      console.log(randomWord[i]);
-    }
+  //Checking if letter exist inside the word
+  if(randomWord.indexOf(userGuesses) > -1) 
+  {
+  //Creating another for loop for new variable that was selected from randomWordSplit iterator    
+    for (var i = 0; i < randomWordSplit.length; i++) {
+      // console.log(randomWordSplit[i]);
 
-    if (keyPressed == randomWord[i]) {
-      console.log(randomWord[i]);
-    } else {
+      //if the randomWordSplit is corrected from user's input on keyboard, that statement is tru
+      if (randomWordSplit[i] === userGuesses) 
+      {
+        underScores[i] = userGuesses; //underScores is equal to userGuesses
+        console.log(underScores);
+      }
     }
+  } //If wrong then push the user's lives decrease
+  else {
+    wrongLetter.push(userGuesses);
+    guessLeft--;
+    console.log(guessLeft);
+  }
+
+}
+}
+
+
+
+  //If key is pressed by user it will compare to the new variable of the randomWordSplit and see if it matches with the string within the word
+  //If the selected input is CORRECTED in the randomWordSplit; than it will input it on the ID selected of html  
+    // if (keyPressed == randomWordSplit[i]) {
+    //   // console.log(keyPressed)
+    // } 
+
+
+
+    
+  //IF isn't corrected, it will grab the ID selector from the HTML to show the losses (counter)
+    // } else if {
+
+
+  
+
+
+
+
+
+
+
 
     //Next steps:
     //create a function pass in the randomWord variable; associate a character in randomWord with each dash up to the length of the dash
     //compare selected word to underscores
     //Style or attach id from div class = hidden word
-  };
-};
+
